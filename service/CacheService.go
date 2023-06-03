6d16cd638aba93f2f52c462b61f24100ec68ec37
Negar <babashahNegar@gmail.com>
2023-06-03 22:07:49 +0330

    Update RedisService
    - Rename it to CacheService

---
+++ service/CacheService.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+var client *redis.Client
+var initialized = false
+
+func startRedisConnection() {
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       3,  // use default DB
+	})
+	initialized = true
+}
+
+func getValue(ctx context.Context, key string) string {
+	if !initialized {
+		startRedisConnection()
+	}
+	val, err := client.Get(ctx, key).Result()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return val
+}
+
+func cacheData(ctx context.Context, key string, value string, expirationDuration time.Duration) {
+	if !initialized {
+		startRedisConnection()
+	}
+	if expirationDuration == -1 {
+		expirationDuration = redis.KeepTTL
+	}
+	err := client.Set(ctx, key, value, expirationDuration).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func deleteValue(ctx context.Context, key string) {
+	if !initialized {
+		startRedisConnection()
+	}
+	err := client.Del(ctx, key).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
