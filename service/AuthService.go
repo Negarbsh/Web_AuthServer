c13d60c89d3189b51d1ba30546e827c6ff623635
Negar <babashahNegar@gmail.com>
2023-06-03 22:08:57 +0330

    Use cache in saving AuthService.getPg response

--- service/AuthService.go
+++ service/AuthService.go
@@ -1,9 +1,11 @@
 package service
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"math/rand"
 	"strings"
+	"time"
 )
 
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -16,6 +18,10 @@ type PgParams struct {
 	messageId   int
 }
 
+func (pg PgParams) String() string {
+	return fmt.Sprintf("%d %d %s %s %d", pg.p, pg.g, pg.nonce, pg.serverNonce, pg.messageId)
+}
+
 type DHParams struct {
 	nonce       string
 	serverNonce string
@@ -23,24 +29,16 @@ type DHParams struct {
 	publicKey   int
 }
 
-func randomString(n int) string {
-	sb := strings.Builder{}
-	sb.Grow(n)
-	for i := 0; i < n; i++ {
-		sb.WriteByte(charset[rand.Intn(len(charset))])
-	}
-	return sb.String()
-}
-
-func randomInt() int {
-	return rand.Int()
-}
-
 func getPg(nonce string, requestMessageId string) PgParams {
-	responseMessageId := 2*randomInt() + 1
+	// generate the response
+	responseMessageId := randomOddInt()
 	serverNonce := randomString(20)
 	pgResponse := PgParams{23, 5, nonce, serverNonce, responseMessageId}
-	// TODO cache response
+
+	// save to cache
+	cacheKey := getCacheKey(nonce, serverNonce)
+	cacheData(nil, cacheKey, pgResponse.String(), 20*time.Minute)
+
 	return pgResponse
 }
 
@@ -55,7 +53,7 @@ func getDHParams(nonce string, serverNonce string, messageId int, requestPublicK
 	fmt.Println(commonKey)
 	// TODO cache common key
 
-	responseMessageId := 2*randomInt() + 1
+	responseMessageId := randomOddInt()
 	dhParams := DHParams{
 		nonce:       nonce,
 		serverNonce: serverNonce,
@@ -63,4 +61,34 @@ func getDHParams(nonce string, serverNonce string, messageId int, requestPublicK
 		publicKey:   responsePublicKey,
 	}
 	return dhParams
+}
+
+func getCacheKey(nonce string, serverNonce string) string {
+	// concat nonce and serverNonce and then hash	 them with sha1
+	combination := nonce + serverNonce
+	return hashWithSHA1(combination)
+}
+
+func randomString(length int) string {
+	sb := strings.Builder{}
+	sb.Grow(length)
+	for i := 0; i < length; i++ {
+		sb.WriteByte(charset[rand.Intn(len(charset))])
+	}
+	return sb.String()
+}
+
+func randomInt() int {
+	return rand.Int()
+}
+
+func randomOddInt() int {
+	return 2*randomInt() + 1
+}
+
+func hashWithSHA1(s string) string {
+	hash := sha1.New()
+	hash.Write([]byte(s))
+	hashInBytes := hash.Sum(nil)
+	return string(hashInBytes)
 }
